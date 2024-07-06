622ec84ecd52b46518e9a0e7ea2d34807f92f093
Sai Satchidanand <sainageswar@gmail.com>
2024-07-06 19:09:47 +0530

    templatize intent prompt

--- src/intent_classification.py
+++ src/intent_classification.py
@@ -6,69 +6,53 @@ Provides few examples (few shot) for model to understand output for input.
 Uses chain of thought by providing interpretation as reasoning for model output.
 """
 
-import re
-import json
-
 from pydantic import BaseModel
 from llama_index.llms.groq import Groq
 from llama_index.core.llms import ChatMessage
 
 
 class IntentResult(BaseModel):
-    context: list[str]
+    query: str
+    contextRequired: list[str]
     interpretation: str
 
 
-system_prompt = """
+class IntentExample(BaseModel):
+    query: str
+    contextRequired: list[str]
+    interpretation: str
+
+
+system_prompt_template = """
 **Contextualizer Prompt**
 
 You are a context expert, and your task is to identify the necessary context required to answer a user's query. 
-You will be provided with a query, and you need to generate a response that includes the following:
+You will be provided with a query, please respond in the following format, replacing placeholders with actual content.
 
-1. **Context**: A list of specific contexts required to answer the query. This can include user-specific 
-    information (e.g., user_profile, scores), web results (e.g., test syllabus, healthy lifestyle), 
-    or other relevant information.
-2. **Interpretation**: A brief explanation of why the specified contexts are required to answer the query.
+query placeholder | list of context required separated by [SEP] placeholder | interpretation placeholder.
 
-Please respond in the following format:
-{ "context": [...list of contexts...], "interpretation": "...brief explanation..." }
+**Important Guidelines**:
+
+1. Be specific and concise in your responses.
+2. Identify the minimum necessary context required to answer the query.
+3. Consider both user-specific information (e.g., user_profile, scores) and external knowledge (e.g., web results) that may be needed to provide a relevant answer.
+4. Provide a clear and concise interpretation of why the specified context is required to answer the query.
+5. Use [SEP] separator for list of context required.
 
 Here are some examples of how this prompt can be used:
+{Examples}
+"""
 
-**Example 1:**
-**Query:** What are my scores?
+context_example_template = """
+**Query:** {query}
 **Response:**
-{ "context": ["user_profile", "scores"], "interpretation": "To convey personalized user scores, scores of 
-    user and profile are required" }
-    
-**Example 2:**
-**Query:** How should I prepare for UPSC?
-**Response:**
-{ "context": ["user_profile", "scores", "web result: UPSC syllabus"], "interpretation": "To help user prepare 
-    for test, current user scores are required and syllabus for test is required from web." }
-    
-**Example 3:**
-**Query:** Which river flows from east to west?
-**Response:**
-{ "context": ["web result: river flowing from east to west"], "interpretation": "Factual question which is 
-    required from the web." }
-    
-**Example 4:**
-**Query:** Civil disobidience movement in India
-**Response:**
-{"context": ["web result: Civil disobidience movement in India"], "interpretation": "Factual question which 
-    is required from the web." }
-
-**Example 5:**
-**Query:** Civil disobidience movement in India
-**Response:**
-{"context": ["web result: Civil disobidience movement in India"], "interpretation": "Factual question which 
-    is required from the web." }
+{query} | {contextRequired} | {interpretation}
 """
 
 
-def few_shot_intent_classification(query: str) -> IntentResult:
+def few_shot_intent_classification(query: str, examples: list[IntentExample]) -> IntentResult:
     llm = Groq(model="llama3-8b-8192")
+    system_prompt = __generate_prompt__(examples)
 
     messages = [
         ChatMessage(role="system", content=system_prompt),
@@ -76,21 +60,28 @@ def few_shot_intent_classification(query: str) -> IntentResult:
     ]
 
     resp = llm.chat(messages)
-    result = resp.message.content
-    result_json = extract_json(result)
+    result_str = resp.message.content
+    result = __extract_intent_result__(result_str)
+    return result
 
-    return IntentResult(**result_json)
+
+def __generate_prompt__(examples: list[IntentExample]) -> str:
+    examples_str = ""
+
+    for example in examples:
+        examples_instance = context_example_template.format(
+            query=example.query,
+            contextRequired="[SEP]".join(example.contextRequired),
+            interpretation=example.interpretation)
+
+        examples_str = examples_str + examples_instance
+
+    result = system_prompt_template.format(Examples=examples_str)
+    return result
 
 
-def extract_json(text: str):
-    match = re.search(r'\{.*\}', text, re.DOTALL)
-    if match:
-        json_str = match.group(0)
-        try:
-            # Parse the JSON response
-            result_json = json.loads(json_str)
-            return result_json
-        except json.JSONDecodeError:
-            raise ValueError("The response from the LLM is not valid JSON.")
-    else:
-        raise ValueError("The response from the LLM does not contain valid JSON.")
+def __extract_intent_result__(text: str) -> IntentResult:
+    result_parts = text.split("|")
+    context_required = result_parts[1].split("[SEP]")
+    result = IntentResult(query=result_parts[0], contextRequired=context_required, interpretation=result_parts[2])
+    return result
--- tests/test_intent_classification.py
+++ tests/test_intent_classification.py
@@ -1,19 +1,90 @@
 import unittest
-from src.intent_classification import few_shot_intent_classification
+from src.intent_classification import few_shot_intent_classification, __generate_prompt__, IntentExample
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
 class TestIntentClassification(unittest.TestCase):
+    def test_generate_prompt(self):
+        generated_prompt = __generate_prompt__(test_intent_examples)
+        self.assertEquals(expected_prompt_template, generated_prompt)
+
     def test_few_shot_intent_classification_withScore(self):
-        result = few_shot_intent_classification("What subjects should I focus today")
+        result = few_shot_intent_classification("How should I set by preparation schedule?", test_intent_examples)
         self.assertIsNotNone(result)
-        self.assertIn("scores", result.context)
+        self.assertIn("scores", result.contextRequired)
 
     def test_few_shot_intent_classification_withWeb(self):
-        result = few_shot_intent_classification("When did India win football world cup last time?")
+        result = few_shot_intent_classification("When did India win football world cup last time?", test_intent_examples)
         self.assertIsNotNone(result)
-        self.assertTrue(len(result.context) > 0)
+        self.assertTrue(len(result.contextRequired) > 0)
         self.assertTrue(result.context[0].startswith("web result"))
 
+    def test_few_shot_intent_classification_withWebAndScore(self):
+        result = few_shot_intent_classification("Explain subject constitution and which topics in constitution should I focus.", test_intent_examples)
+        self.assertIsNotNone(result)
+        self.assertTrue(len(result.contextRequired) > 1)
+        self.assertIn("scores", result.contextRequired)
+
+
+test_intent_examples = [
+    IntentExample(query="What are my scores?", contextRequired=["user_profile", "scores"],
+                  interpretation="To convey personalized user scores, scores of user and profile are required"),
+
+    IntentExample(query="How should I prepare for UPSC?",
+                  contextRequired=["user_profile", "scores", "web result: UPSC syllabus"],
+                  interpretation="To help user prepare for test, current user scores are required and syllabus for test is required from web."),
+
+    IntentExample(query="Which river flows from east to west?",
+                  contextRequired=["web result: river flowing from east to west"],
+                  interpretation="Factual question which is required from the web."),
+
+    IntentExample(query="Civil disobidience movement in India",
+                  contextRequired=["web result: Civil disobidience movement in India"],
+                  interpretation="Factual question which is required from the web."),
+
+    IntentExample(query="Which subjects should I focus on?",
+                  contextRequired=["user_profile", "scores", "web result: UPSC syllabus"],
+                  interpretation="To suggest subjects to focus, current user scores are required to know user's strengths and weakness. Further, UPSC syllabus is required to suggest study schedule.")
+]
+
+expected_prompt_template = """
+**Contextualizer Prompt**
+
+You are a context expert, and your task is to identify the necessary context required to answer a user's query. 
+You will be provided with a query, please respond in the following format, replacing placeholders with actual content.
+
+query placeholder | list of context required separated by [SEP] placeholder | interpretation placeholder.
+
+**Important Guidelines**:
+
+1. Be specific and concise in your responses.
+2. Identify the minimum necessary context required to answer the query.
+3. Consider both user-specific information (e.g., user_profile, scores) and external knowledge (e.g., web results) that may be needed to provide a relevant answer.
+4. Provide a clear and concise interpretation of why the specified context is required to answer the query.
+5. Use [SEP] separator for list of context required.
+
+Here are some examples of how this prompt can be used:
+
+**Query:** What are my scores?
+**Response:**
+What are my scores? | user_profile[SEP]scores | To convey personalized user scores, scores of user and profile are required
+
+**Query:** How should I prepare for UPSC?
+**Response:**
+How should I prepare for UPSC? | user_profile[SEP]scores[SEP]web result: UPSC syllabus | To help user prepare for test, current user scores are required and syllabus for test is required from web.
+
+**Query:** Which river flows from east to west?
+**Response:**
+Which river flows from east to west? | web result: river flowing from east to west | Factual question which is required from the web.
+
+**Query:** Civil disobidience movement in India
+**Response:**
+Civil disobidience movement in India | web result: Civil disobidience movement in India | Factual question which is required from the web.
+
+**Query:** Which subjects should I focus on?
+**Response:**
+Which subjects should I focus on? | user_profile[SEP]scores[SEP]web result: UPSC syllabus | To suggest subjects to focus, current user scores are required to know user's strengths and weakness. Further, UPSC syllabus is required to suggest study schedule.
+
+"""
